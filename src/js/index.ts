7f9cd5385c2458876b8e9e0d3d5dd07a51ad437a
lucalarsen <lucalarsen@hotmail.com>
2019-03-19 08:57:37 +0100

    dårligt lavet men stadig lavet

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,56 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+document.getElementById("udregnbtnbil").addEventListener("click",udregnPrisBil);
+document.getElementById("udregnbtnelbil").addEventListener("click",udregnPrisElbil);
+document.getElementById("slet").addEventListener("click",slet);
+
+let elementInput = document.getElementById("prisenPaaBil");
+let personbil: number = 200000;
+
+
+function udregnPrisBil() {
+    let inputText: number = parseInt((<HTMLInputElement>elementInput).value);
+    var gammelpris = document.getElementById('content');
+    var nypris = document.createElement('li');
+
+    if (inputText <= personbil) {
+        let resultat: number = inputText * 0.85;
+        let nyText = document.createTextNode(resultat.toString());
+        gammelpris.appendChild(nypris);
+        nypris.appendChild(nyText);
+
+        }
+    else{
+        let resultat: number = (inputText * 1.50)-130000;
+        let nyText = document.createTextNode(resultat.toString());
+        gammelpris.appendChild(nypris);
+        nypris.appendChild(nyText);
+        
+        }
+}
+function udregnPrisElbil() {
+    let inputText: number = parseInt((<HTMLInputElement>elementInput).value);
+    var gammelpris = document.getElementById('content');
+    var nypris = document.createElement('li');
+
+
+    if (inputText <= personbil) {
+        let resultat: number = (inputText * 0.85)*0.20;
+        let nyText = document.createTextNode(resultat.toString());
+        gammelpris.appendChild(nypris);
+        nypris.appendChild(nyText);
+
+        }
+    else{
+        let resultat: number = ((inputText * 1.50)-130000)*0.20;
+        let nyText = document.createTextNode(resultat.toString());
+        gammelpris.appendChild(nypris);
+        nypris.appendChild(nyText);
+        
+        }
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function slet() {
+    let item = document.getElementById("content");
+    while (item.hasChildNodes()) {
+        item.removeChild(item.firstChild);
+    }
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
